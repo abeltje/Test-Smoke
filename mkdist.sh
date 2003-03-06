658bf618d6140c69ac15fd2a7c4c380018055a01
abeltje <abeltje@f9e6db9c-56bf-0310-a82a-abbfb51efdf0>
2003-03-06 01:07:40 +0000

    Added some echo commands to make it clearer
    git-svn-id: https://source.test-smoke.org/svn/Test-Smoke@17 f9e6db9c-56bf-0310-a82a-abbfb51efdf0

--- mkdist.sh
+++ mkdist.sh
@@ -1,10 +1,16 @@
 #! /bin/sh
 
+echo 'Check compile'
 perl test_compile.pl || exit
+
+echo 'Check POD'
 perl test_pod.pl     || exit
+
+echo 'Create Makefile'
 perl Makefile.PL
 make
 (make test) || exit
 make dist
 mv *.tar.gz ../
+
 make veryclean
