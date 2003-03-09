19ab40b75095243e1b36c1bed5c49cfc125648f8
abeltje <abeltje@f9e6db9c-56bf-0310-a82a-abbfb51efdf0>
2003-03-09 19:48:14 +0000

    restore the original
    git-svn-id: https://source.test-smoke.org/svn/Test-Smoke@39 f9e6db9c-56bf-0310-a82a-abbfb51efdf0

--- mkdist.sh
+++ mkdist.sh
@@ -1,18 +1,10 @@
 #! /bin/sh
 
-echo 'Check compile'
 perl test_compile.pl || exit
-
-echo 'Check POD'
 perl test_pod.pl     || exit
-
-echo 'Create Makefile'
 perl Makefile.PL
 make
 (make test) || exit
-
-echo 'Create distribution'
-make dist 
+make dist
 mv *.tar.gz ../
-
 make veryclean
