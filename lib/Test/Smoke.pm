27a14481dd5d98d93dd77e3069dbfe65c641a0ca
abeltje <abeltje@f9e6db9c-56bf-0310-a82a-abbfb51efdf0>
2003-03-11 06:28:49 +0000

    * Get the name section right
    git-svn-id: https://source.test-smoke.org/svn/Test-Smoke@58 f9e6db9c-56bf-0310-a82a-abbfb51efdf0

--- lib/Test/Smoke.pm
+++ lib/Test/Smoke.pm
@@ -2,7 +2,7 @@ package Test::Smoke;
 use strict;
 
 use vars qw( $VERSION $conf @EXPORT );
-$VERSION = '1.16_24';
+$VERSION = '1.16_25';
 
 use base 'Exporter';
 @EXPORT  = qw( $conf &read_config );
@@ -11,7 +11,7 @@ my $ConfigError;
 
 =head1 NAME
 
-Test::Smoke - Centralise $VERSION and handle reading the config
+Test::Smoke - The perl core test smoke suite
 
 =head1 SYNOPSIS
 
