f20f8284efc3a15c67759bbd265957f27605becb
abeltje <abeltje@f9e6db9c-56bf-0310-a82a-abbfb51efdf0>
2003-03-14 21:06:23 +0000

    Cope with the new F<Makefile.PL>
    git-svn-id: https://source.test-smoke.org/svn/Test-Smoke@71 f9e6db9c-56bf-0310-a82a-abbfb51efdf0

--- mkdist.sh
+++ mkdist.sh
@@ -2,6 +2,9 @@
 
 perl test_compile.pl || exit
 perl test_pod.pl     || exit
+PERL_MM_USE_DEFAULT=y
+export PERL_MM_USE_DEFAULT
+echo Set default input: $PERL_MM_USE_DEFAULT
 perl Makefile.PL
 make
 (make test) || exit
