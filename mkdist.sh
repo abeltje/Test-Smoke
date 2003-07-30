60aadf794255a5d16214a531a5f6f3fd1d16e870
abeltje <abeltje@f9e6db9c-56bf-0310-a82a-abbfb51efdf0>
2003-07-30 18:20:32 +0000

    * Reorganize the private tests and make-dist tool
    git-svn-id: https://source.test-smoke.org/svn/Test-Smoke@291 f9e6db9c-56bf-0310-a82a-abbfb51efdf0

--- mkdist.sh
+++ mkdist.sh
@@ -1,20 +1,35 @@
 #! /bin/sh
 
-distdir=~/distro
+# Set the directory where distributions are kept
+distdir=./
+if [ "`uname -n`" == "fikkie" ] ; then
+    distdir=~/distro
+fi
 
-perl test_compile.pl || exit
-perl test_pod.pl     || exit
+
+# Check if all the distributed perl-files compile
+perl private/test_compile.pl || exit
+
+# Check if all the distibuted files with POD are pod_ok
+perl private/test_pod.pl     || exit
+
+# Run the private Test::Smoke::Smoker tests
 cd private
 perl -MTest::Harness -e 'runtests( @ARGV, 1)' smoker_*.t || (cd .. ; exit)
-#perl smoker_mini.t || (cd .. ; exit)
 cd ..
+
+# Now create the Makefile and and run the public test-suite
 PERL_MM_USE_DEFAULT=y
 export PERL_MM_USE_DEFAULT
 echo Set default input: $PERL_MM_USE_DEFAULT
 perl Makefile.PL
 make
 (make test) || exit
+
+# Create the distribution and move it to the distribution directory
 make dist
 mv -v *.tar.gz $distdir
+
+# Clean up!
 make veryclean > /dev/null
 rm -f */*/*/*~
