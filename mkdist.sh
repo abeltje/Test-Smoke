8e6eb11495dc8afa802ad41318df30b28b150dc4
abeltje <abeltje@f9e6db9c-56bf-0310-a82a-abbfb51efdf0>
2003-07-31 20:55:52 +0000

    * Make mkdist.sh a bit more flexible
    git-svn-id: https://source.test-smoke.org/svn/Test-Smoke@296 f9e6db9c-56bf-0310-a82a-abbfb51efdf0

--- mkdist.sh
+++ mkdist.sh
@@ -1,11 +1,15 @@
 #! /bin/sh
 
+# $Id$
 # Set the directory where distributions are kept
 distdir=./
 if [ "`uname -n`" == "fikkie" ] ; then
     distdir=~/distro
 fi
-
+if [ "$SMOKE_DIST_DIR" != "" ] ; then
+    distdir=$SMOKE_DIST_DIR
+fi
+echo "Will put the distribution in: '$distdir'"
 
 # Check if all the distributed perl-files compile
 perl private/test_compile.pl || exit
@@ -15,8 +19,15 @@ perl private/test_pod.pl     || exit
 
 # Run the private Test::Smoke::Smoker tests
 cd private
-perl -MTest::Harness -e 'runtests( @ARGV, 1)' smoker_*.t || (cd .. ; exit)
+for test in smoker_*.t; do
+    perl $test 2>/dev/null || (cd .. ; exit)
+done
 cd ..
+
+if [ "$SMOKE_TEST_ONLY" == "1" ] ; then
+    echo "SMOKE_TEST_ONLY was set, quitting..."
+    exit
+fi
 
 # Now create the Makefile and and run the public test-suite
 PERL_MM_USE_DEFAULT=y
@@ -24,7 +35,7 @@ export PERL_MM_USE_DEFAULT
 echo Set default input: $PERL_MM_USE_DEFAULT
 perl Makefile.PL
 make
-(make test) || exit
+(SMOKE_SKIP_SIGTEST=1 make test) || exit
 
 # Create the distribution and move it to the distribution directory
 make dist
