908f754b01a3aecd576b78b187ecbd269856e0ce
abeltje <abeltje@f9e6db9c-56bf-0310-a82a-abbfb51efdf0>
2004-01-12 22:27:48 +0000

    * [FIX] By Merijn: Date: Mon, 12 Jan 2004 20:06:08 +0100 From: "H.Merijn Brand" <h.m.brand@hccnet.nl> Subject: Re: Smoke [5.9.0] 22104 FAIL(Fc) aix 3 (004ca4aa4c00/1 cpu)
    reporter is mijn schuld. HP-UX < 11.00 (dit is 10.20) kan zowiezo geen 64 bit
    Meaning: HP-UX before 11.00 doesn't do 64-bit
    
    
    git-svn-id: https://source.test-smoke.org/svn/Test-Smoke@557 f9e6db9c-56bf-0310-a82a-abbfb51efdf0

--- lib/Test/Smoke/SysInfo.pm
+++ lib/Test/Smoke/SysInfo.pm
@@ -263,15 +263,18 @@ sub HPUX {
             push @cpu, $_;
         }
     }
-    $hpux->{_os} =~ s/ B\./ /;
+    $hpux->{_os} =~ s/ B\.(\d+)/ $1/;
+    my $os_r = $1;
 
-    chomp( my $k64 = `getconf KERNEL_BITS` );
-    $k64 and $hpux->{_os} .= "/$k64";
+    if ($os_r >= 11) {
+       chomp( my $k64 = `getconf KERNEL_BITS` );
+       $k64 and $hpux->{_os} .= "/$k64";
+    }
 
     if ($cpu[0] =~ m/^\S+\s+(\d+\.\d+)\s+(\S+)/) {
 	my( $arch, $cpu ) = ("PA-$1", $2);
 	$hpux->{_cpu} = $cpu;
-	$hpux->{_cpu_type} = `getconf HW_32_64_CAPABLE` =~ m/^1/ 
+        $hpux->{_cpu_type} = $os_r >= 11 && `getconf HW_32_64_CAPABLE` =~ m/^1/
             ? "$arch/64" : "$arch/32";
     }
     return $hpux;
