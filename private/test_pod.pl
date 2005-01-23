d651253b58c2cd1d677965b4e1abbee428acdfde
abeltje <abeltje@f9e6db9c-56bf-0310-a82a-abbfb51efdf0>
2005-01-23 10:40:38 +0000

    * [PRIVATE]   * FAQ moved to Test::Smoke::FAQ
    git-svn-id: https://source.test-smoke.org/svn/Test-Smoke@814 f9e6db9c-56bf-0310-a82a-abbfb51efdf0

--- private/test_pod.pl
+++ private/test_pod.pl
@@ -10,13 +10,13 @@ my @test_files;
 BEGIN {
     @test_files = qw(
         synctree.pl patchtree.pl mailrpt.pl
-        smokeperl.pl configsmoke.pl README ReleaseNotes FAQ runsmoke.pl
+        smokeperl.pl configsmoke.pl README ReleaseNotes runsmoke.pl
         archiverpt.pl smokestatus.pl W32Configure.pl
     );
     push @test_files, map File::Spec->catfile( 'lib', 'Test', 'Smoke', $_ )
         => qw( Syncer.pm SourceTree.pm Policy.pm Util.pm 
                SysInfo.pm Reporter.pm FTPClient.pm
-               Patcher.pm Mailer.pm BuildCFG.pm Smoker.pm );
+               Patcher.pm Mailer.pm BuildCFG.pm Smoker.pm FAQ );
     push @test_files, map File::Spec->catfile( 'lib', 'Test', $_ )
         => qw( Smoke.pm );
 }
