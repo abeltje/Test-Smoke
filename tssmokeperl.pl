cca672f5bba94c718377c2d5afa5ba0276e173d6
abeltje <abeltje@test-smoke.org>
2013-05-31 08:26:46 +0200

    Fallout of the first testround with 1.61-RC1

--- tssmokeperl.pl
+++ tssmokeperl.pl
@@ -1,6 +1,8 @@
 #! /usr/bin/perl -w
 use strict;
 
+use FindBin;
+use lib $FindBin::Bin;
 use lib 'lib';
 
 use Test::Smoke::App::Options;
@@ -10,7 +12,7 @@ my $app = Test::Smoke::App::SmokePerl->new(
     Test::Smoke::App::Options->smokeperl_config()
 );
 
-if (my $error = $app->configfile_erro) {
+if (my $error = $app->configfile_error) {
     die "$error\n";
 }
 
--- tssynctree.pl
+++ tssynctree.pl
@@ -1,6 +1,8 @@
 #! /usr/bin/perl -w
 use strict;
 
+use FindBin;
+use lib $FindBin::Bin;
 use lib 'lib';
 
 use Test::Smoke::App::Options;
