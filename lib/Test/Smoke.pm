eca771794d6e4aa8db8d46839f01e68849594a24
abeltje <abeltje@test-smoke.org>
2016-05-04 00:19:12 +0200

    Autocommit for distribution Test::Smoke 1.70_01 (test)

--- lib/Test/Smoke.pm
+++ lib/Test/Smoke.pm
@@ -2,7 +2,7 @@ package Test::Smoke;
 use strict;
 
 use vars qw($VERSION $conf @EXPORT);
-$VERSION  = '1.70';
+$VERSION  = '1.70_01';
 
 use base 'Exporter';
 @EXPORT  = qw( $conf &read_config &run_smoke );
