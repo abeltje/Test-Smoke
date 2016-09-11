1db21a4b49b3c6a7c396333379dbb585e17d7497
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2016-09-11 14:16:38 +0200

    Note DISTRIB_RELEASE in SysInfo
    (Also available in System::Info)

--- lib/Test/Smoke/SysInfo/Linux.pm
+++ lib/Test/Smoke/SysInfo/Linux.pm
@@ -90,8 +90,10 @@ sub prepare_os {
 
     if ( $os{DISTRIB_DESCRIPTION} ) {
         $distro = $os{DISTRIB_DESCRIPTION};
-        $os{DISTRIB_CODENAME} && $distro !~ m{\b$os{DISTRIB_CODENAME}\b} and
+        $os{DISTRIB_CODENAME} && $distro !~ m{\b$os{DISTRIB_CODENAME}\b}i and
             $distro .= " ($os{DISTRIB_CODENAME})";
+        $os{DISTRIB_RELEASE}  && $distro !~ m{\b$os{DISTRIB_RELEASE}\b}i  and
+            $distro .= " ($os{DISTRIB_RELEASE})";
     }
     elsif ( $os{PRETTY_NAME} ) {
         $distro = $os{PRETTY_NAME};          # "openSUSE 12.1 (Asparagus) (x86_64)"
