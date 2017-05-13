f958884df6a1e1d18c939c8980b57b3efb289c81
abeltje <abeltje@test-smoke.org>
2017-05-13 16:00:41 +0200

    Fix get_avail_w32compilers() by Steve Hay:
    Test-Smoke-1.42 fails to detect the ccversion for my MSVC++
    installations, which all reside under "C:\Program Files\...". It tries
    to run a command like
    
    "$map{ $CC }->{ccbin}" --version 2>&1
    
    but the 'ccbin' there comes from whereis() and is already double-quoted
    if it contains spaces, which causes an error like
    
    '""C:\Program' is not recognized as an internal or external command,
    operable program or batch file.
    
    The attached patch fixes this.
    
    https://rt.cpan.org/Ticket/Display.html?id=54542

--- lib/Test/Smoke/Util/FindHelpers.pm
+++ lib/Test/Smoke/Util/FindHelpers.pm
@@ -189,7 +189,7 @@ sub get_avail_w32compilers {
     my $CC = 'MSVC';
     if ( $map{ $CC }->{ccbin} = whereis( $map{ $CC }->{ccname} ) ) {
         # No, cl doesn't support --version (One can but try)
-        my $output =`"$map{ $CC }->{ccbin}" --version 2>&1`;
+        my $output =`$map{ $CC }->{ccbin} --version 2>&1`;
         my $ccvers = $output =~ /^.*Version\s+([\d.]+)/ ? $1 : '?';
         $map{ $CC }->{ccversarg} = "ccversion=$ccvers";
         my $mainvers = $ccvers =~ /^(\d+)/ ? $1 : 1;
@@ -199,7 +199,7 @@ sub get_avail_w32compilers {
     $CC = 'BCC';
     if ( $map{ $CC }->{ccbin} = whereis( $map{ $CC }->{ccname} ) ) {
         # No, bcc32 doesn't support --version (One can but try)
-        my $output = `"$map{ $CC }->{ccbin}" --version 2>&1`;
+        my $output = `$map{ $CC }->{ccbin} --version 2>&1`;
         my $ccvers = $output =~ /([\d.]+)/ ? $1 : '?';
         $map{ $CC }->{ccversarg} = "ccversion=$ccvers";
         $map{ $CC }->{CCTYPE} = 'BORLAND';
@@ -210,7 +210,7 @@ sub get_avail_w32compilers {
         local *STDERR;
         open STDERR, ">&STDOUT"; #do we need an error?
         select( (select( STDERR ), $|++ )[0] );
-        my $output = `"$map{ $CC }->{ccbin}" --version`;
+        my $output = `$map{ $CC }->{ccbin} --version`;
         my $ccvers = $output =~ /(\d+.*)/ ? $1 : '?';
         $ccvers =~ s/\s+copyright.*//i;
         $map{ $CC }->{ccversarg} = "gccversion=$ccvers";
