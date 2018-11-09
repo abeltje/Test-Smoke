6e9baf9ccbcff32aeea8fd75a9ad1565c43b32b0
Abe Timmerman <abeltje@cpan.org>
2018-11-09 14:16:31 +0100

    Autocommit for distribution Test::Smoke 1.72_06 (same)

--- lib/Test/Smoke.pm
+++ lib/Test/Smoke.pm
@@ -2,7 +2,7 @@ package Test::Smoke;
 use strict;
 
 use vars qw($VERSION $conf @EXPORT);
-$VERSION  = '1.72_05';
+$VERSION  = '1.72_06';
 
 use base 'Exporter';
 @EXPORT  = qw( $conf &read_config &run_smoke );
