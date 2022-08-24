1d9a306329a28c28113eac2fba172a024e5ea1ac
Abe Timmerman <abeltje@cpan.org>
2022-08-24 14:10:34 +0200

    Autocommit for distribution Test::Smoke 1.79_04 (test)

--- lib/Test/Smoke.pm
+++ lib/Test/Smoke.pm
@@ -2,7 +2,7 @@ package Test::Smoke;
 use strict;
 
 use vars qw($VERSION $conf @EXPORT);
-$VERSION  = '1.79_03';
+$VERSION  = '1.79_04';
 
 use base 'Exporter';
 @EXPORT  = qw( $conf &read_config &run_smoke );
